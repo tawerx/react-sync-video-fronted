a02f2075cc20261c8b01bbc1a719cae671fcabb6
Дима Бурлаков <tawerxo@mail.ru>
2022-07-27 08:55:29 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const videoRef = React.useRef();
   const [socket, setSocket] = React.useState(0);
   React.useEffect(() => {
-    setSocket(io('https://react-sync-video.herokuapp.com/'));
+    setSocket(io(process.env.REACT_APP_API_URL));
   }, []);
 
   const onClickSync = () => {
