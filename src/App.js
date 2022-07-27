21997161b3ad1e7cd2f73fb440adf16291020e71
Дима Бурлаков <tawerxo@mail.ru>
2022-07-27 08:48:15 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { io } from 'socket.io-client';
+import Video from './assets/video/sintel-short.mp4';
+
+function App() {
+  const videoRef = React.useRef();
+  const [socket, setSocket] = React.useState(0);
+  React.useEffect(() => {
+    setSocket(io('http://localhost:3001/'));
+  }, []);
+
+  const onClickSync = () => {
+    const currentTime = videoRef.current.currentTime;
+    socket.emit('currentTime', currentTime);
+  };
+
+  if (videoRef.current) {
+    socket.on('currentTime', (time) => {
+      videoRef.current.pause();
+      videoRef.current.currentTime = time;
+      videoRef.current.play();
+    });
+  }
+
+  return (
+    <div className="App">
+      <video ref={videoRef} src={Video} width="900" height="500" controls="controls" />
+      <button onClick={onClickSync}>Синхронизировать</button>
+    </div>
+  );
+}
+
+export default App;
